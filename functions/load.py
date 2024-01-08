e8d7921b52b49af7572a83ca3ded5a11afda4100
benjamin <Benjaminmk06@gmail.com>
2024-01-08 10:25:53 +0100

    OOP Update
    I guess better implementation (enhanced performance) even tho it doesnt really need it :laughing:

--- functions/load.py
+++ functions/load.py
@@ -1,14 +1,15 @@
 import os
 
-def load():
-    virkhistory_file = 'history.txt'
+class Load:
+    def run(self):
+        virkhistory_file = 'history.txt'
 
-    if not os.path.exists(virkhistory_file):
-        print('No search history found.')
-        return
+        if not os.path.exists(virkhistory_file):
+            print('No search history found.')
+            return
 
-    with open(virkhistory_file, 'r') as f:
-        content = f.read()
-        content = content.replace('%20', ' ')
-        print('Search history:')
-        print(content)
+        with open(virkhistory_file, 'r') as f:
+            content = f.read()
+            content = content.replace('%20', ' ')
+            print('Search history:')
+            print(content)
--- main.py
+++ main.py
@@ -1,20 +1,27 @@
+# main.py
 from utils.misc import *
 from utils.imports import *
-def main():
-    while True:
-        os.system("cls")
-        menu()
-        user_input = input("Indtast dit valg: ")
-        if user_input == "1":
-            from functions.search import search
-            search()
-            break
-        elif user_input == "2":
-            from functions.load import load
-            load()
-            break
-        else:
-            print("Ugyldigt valg!")
+from functions.search import Search
+from functions.load import Load
+
+class Main:
+    def __init__(self):
+        self.search = Search(self)
+        self.load = Load()
+
+    def run(self):
+        while True:
+            os.system("cls")
+            menu()
+            user_input = input("Indtast dit valg: ")
+            if user_input == "1":
+                self.search.run()
+            elif user_input == "2":
+                self.load.run()
+            elif user_input == "11":
+                continue
+            else:
+                print("Ugyldigt valg!")
 
 if __name__ == "__main__":
-    main()  
+    Main().run()
